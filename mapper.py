5257de14345ce7a08882d788e7d59276ebaf9f6a
Steve Dominguez <steve@dominguezhouse.net>
2020-05-13 15:54:52 +0000

    added more stopwords and word-length limits

--- mapper.py
+++ mapper.py
@@ -4,6 +4,8 @@ import string
 from sklearn.feature_extraction import stop_words
 
 stops = set(stop_words.ENGLISH_STOP_WORDS)
+stops.update('0', '1', '2', '3', '4', '5', '6', '7', '8', '9') # add digits to stopwords (brute force way)
+stops.update(['thou', 'shall', 'thee']) # notice, update needs a formal "set", or else it will add each single character...
 
 # get all lines from stdin
 for line in sys.stdin:
@@ -19,5 +21,6 @@ for line in sys.stdin:
 
     # output tuples (word, 1) in tab-delimited format, but only if they're not a stopword!
     for word in words:
-        if word not in stops:
-            print '%s\t%s' % (word, "1")
+        if word not in stops: # omit stopwords
+            if len(word) > 3: # ignore words shorter than 3 characters
+                print '%s\t%s' % (word, "1")
